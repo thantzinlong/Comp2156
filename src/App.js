c494507ec7fd1d83b6198536560e30fafeb626a2
thantzinlong <jokertzw247@gmail.com>
2025-02-09 16:09:59 -0500

    including application source

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+import React, { useState } from "react";
+import StudentForm from "./components/StudentForm";
+import StudentList from "./components/StudentList";
+
+const App = () => {
+  const [students, setStudents] = useState([]);
+
+  const addStudent = (student) => {
+    setStudents([...students, student]);
+  };
+
+  return (
+    <div className="App">
+      <h1>Studen ID: 101494060</h1>
+      <h1>Studen Name: Thant Zin Win</h1>
+      <h1>Student Information System</h1>
+      <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
+      <StudentForm addStudent={addStudent} />
+      Developerd By: Thant Zin Win
+      <StudentList students={students} />
+    </div>
+  );
+};
+
+export default App;
